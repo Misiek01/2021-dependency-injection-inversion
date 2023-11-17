497f7c74fae7547078bba0be86eb3b8223d3de6a
Michał Drab <MIDRA@orsted.com>
2023-11-17 14:41:04 +0100

    'pytest_p1'

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -1,8 +1,10 @@
+#fixture
 import pytest
-
 @pytest.fixture
 def output_fixture():
     return 4
 
-def test_multiplication_of_two_times_two_equals_four(output_fixture):
-    assert 2*2==output_fixture
+
+def test_multiplication_of_two_times_two_equal_four(output_fixture):
+    assert 2*2==output_fixture  # jak tak wpiszemy w konsoli to manual test
+    
